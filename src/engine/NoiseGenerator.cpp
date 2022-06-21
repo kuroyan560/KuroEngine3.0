34271be661a6297e9803580b36f92bc5b609d4e3
hassy560 <k020g1167@g.neec.ac.jp>
2022-06-21 23:32:11 +0900

    ノイズクラス

---
+++ src/engine/NoiseGenerator.cpp
@@ -0,0 +1,5 @@
+#include "NoiseGenerator.h"
+
+void NoiseGenerator::PerlinNoise(const Vec2<int>& Size, const float& Split)
+{
+}
---
+++ src/engine/NoiseGenerator.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"Vec.h"
+class NoiseGenerator
+{
+	void PerlinNoise(const Vec2<int>& Size, const float& Split);
+};
+
